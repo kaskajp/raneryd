e6ecb3e9d2586b70410bbb418c1522f2b8d9c089
Jonas <jonas@kaska.jp>
2018-07-14 23:28:35 +0200

    Accessibility fix

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 'use strict';
-const VERSION = 'cache-v40180621093773+0000';
+const VERSION = 'cache-v40180621093779+0000';
 const urls = ['/', '/pages/about', '/pages/contact', '/robots.txt', '/manifest.json', '/assets/images/icon-128x128.png', '/assets/images/icon-144x144.png', '/assets/images/icon-152x152.png', '/assets/images/icon-192x192.png', '/assets/images/icon-512x512.png', 'https://avatars3.githubusercontent.com/u/1626458?s=460&v=4'];
 self.addEventListener('install', event => {
     event.waitUntil(caches.open(VERSION).then(cache => cache.addAll(urls)));
